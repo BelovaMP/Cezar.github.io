b2f1fa71509e53b60cae56fbcdad17995424dfdc
BelovaMP <83486459+BelovaMP@users.noreply.github.com>
2021-05-01 16:23:03 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+var words = ["время","оттепель","волк","крупа","канарейка"];
+var alfa = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+var word = "";
+function randomInteger(min, max) 
+{
+  var rand = min - 0.5 + Math.random() * (max - min + 1);
+  return Math.round(rand);
+}
+function scrambler()
+{	
+	var keyValue = randomInteger(1,5);
+	var cipherString = "";
+	word = String(words[randomInteger(0, words.length - 1)]);
+	for (var j = 0; j < word.length; j++) 
+	{
+		for(var k = 0; k < alfa.length; k++)
+		{
+			if(word[j]==alfa[k])
+			{
+				if((k + keyValue) <= alfa.length)
+				{
+					cipherString += alfa[k + keyValue];
+				}	
+				else if ((k + keyValue) > alfa.length)
+				{
+					cipherString += alfa[k + keyValue - alfa.length];
+				}
+			}
+		}
+	}
+	key.insertAdjacentHTML("afterbegin", `<div class="keyValue">${keyValue}</div>`);
+	cipher.insertAdjacentHTML("afterbegin", `<div class="keyValue">${cipherString}</div>`);
+}
+function correctDecipherment()
+{
+	if(document.getElementById("input_decryption").value == word)
+	{
+		document.getElementById("res").innerHTML = "Правильно!";
+		document.getElementById("res").style.background = "green"; 	
+	}
+	else
+	{
+		document.getElementById("res").innerHTML = "Неправильно!";
+		document.getElementById("res").style.background = "red";	
+	}
+}
